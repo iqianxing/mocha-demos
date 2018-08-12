692b6be380edda3999d4216a6ee54cd6bfb72e1c
iqianxing <qianxingqianli@qq.com>
2018-08-12 11:40:51 +0800

    增加中文对比

--- mocha-helloworld/test/jsondiff.test.js
+++ mocha-helloworld/test/jsondiff.test.js
@@ -24,4 +24,16 @@ describe("JSON对比测试", function () {
     var newJson = JSON.parse('{"success":true,"hello":"world","user":{"name":"test","age":10}}');
     assert.deepEqual(oldJson, newJson);
   });
+
+  it("中文字符", function () {
+    var oldJson = JSON.parse('{"hello":"你好","success":true}');
+    var newJson = JSON.parse('{"success":true,"hello":"你好吗"}');
+    assert.deepEqual(oldJson, newJson);
+  });
+
+  it("日期", function () {
+    var oldJson = JSON.parse('{"hello":"你好","success":true,"birthDay":"1995-06-15 10:05:00"}');
+    var newJson = JSON.parse('{"success":true,"hello":"你好","birthDay":"1995-06-15 20:05:00"}');
+    assert.deepEqual(oldJson, newJson);
+  });
 })
