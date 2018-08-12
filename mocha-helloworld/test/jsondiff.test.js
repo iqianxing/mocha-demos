f68d75adb4816ee1a72c37bf89ea28355c2c9683
iqianxing <qianxingqianli@qq.com>
2018-08-12 10:34:30 +0800

    修改jsondiff

--- mocha-helloworld/test/jsondiff.test.js
+++ mocha-helloworld/test/jsondiff.test.js
@@ -1,15 +1,27 @@
 var assert = require("chai").assert;
 
-describe("json diff", function () {
-  it("json相同", function () {
-    var oldJson = JSON.parse('{"title":"com.tencent.news","fullTitle":"Androbugs report com.tencent.news","timedOut":false,"duration":0,"state":"passed","speed":"fast","pass":true,"fail":false,"pending":false}')
-    var newJson = JSON.parse('{"title":"com.tencent.news","fullTitle":"Androbugs report com.tencent.news","timedOut":false,"duration":0,"state":"passed","speed":"fast","pass":true,"fail":false,"pending":false}')
-    assert.deepEqual(oldJson, newJson)
-  })
+describe("JSON对比测试", function () {
+  it("Key顺序相同+Value相同", function () {
+    var oldJson = JSON.parse('{"hello":"world","success":true}');
+    var newJson = JSON.parse('{"hello":"world","success":true}');
+    assert.deepEqual(oldJson, newJson);
+  });
 
-  it("json不同", function () {
-    var oldJson = JSON.parse('{"title":"com.tencent.news","fullTitle":"Androbugs report com.tencent.news","timedOut":false,"duration":0,"state":"passed","speed":"fast","pass":true,"fail":false,"pending":false}')
-    var newJson = JSON.parse('{"title":"com.tencent.news2","fullTitle":"Androbugs report com.tencent.news","timedOut":false,"duration":0,"state":"passed","speed":"fast","pass":true,"fail":false,"pending":false}')
-    assert.deepEqual(oldJson, newJson)
-  })
+  it("Key顺序不同+Value相同", function () {
+    var oldJson = JSON.parse('{"hello":"world","success":true}');
+    var newJson = JSON.parse('{"success":true,"hello":"world"}');
+    assert.deepEqual(oldJson, newJson);
+  });
+
+  it("Key顺序不同+Value不同", function () {
+    var oldJson = JSON.parse('{"hello":"world","success":true}');
+    var newJson = JSON.parse('{"success":true,"hello":"wOrld"}');
+    assert.deepEqual(oldJson, newJson);
+  });
+  
+  it("JSON嵌套：Key顺序不同+Value不同", function () {
+    var oldJson = JSON.parse('{"user":{"name":"devops","age":10},"hello":"world","success":true}');
+    var newJson = JSON.parse('{"success":true,"hello":"wOrld","user":{"name":"test","age":10}}');
+    assert.deepEqual(oldJson, newJson);
+  });
 })
