1a672c2fede105a402536132316d2665814baca8
iqianxing <qianxingqianli@qq.com>
2018-09-02 13:43:07 +0800

    遍历数据库中数据

--- mocha-helloworld/test/mysqlStoredCases.test.js
+++ mocha-helloworld/test/mysqlStoredCases.test.js
@@ -1,6 +1,6 @@
 var assert = require("chai").assert;
 var mysql = require("mysql");
-var Promise = require("bluebird");
+var request = require("request");
 
 module.exports = function (testcases) {
   describe("# 动态生成用例", function () {
@@ -10,10 +10,35 @@ module.exports = function (testcases) {
           assert.ok(1);
         });
 
-        it("异步用例", function (done) {
-          assert.ok(1);
-          setTimeout(done, 200)
+        it("检测页面是否存活", function (done) {
+          if (!testcase.homepage) {
+            this.skip();
+          }
+          request.get(testcase.homepage, function (error, response, body) {
+            assert.equal(response.statusCode, 200);
+            done();
+          })
         });
+
+        it("检查mysql数据库中数据", function (done) {
+          var connection = mysql.createConnection({
+            host: '127.0.0.1',
+            user: 'root',
+            password: '',
+            database: 'test'
+          });
+          connection.connect();
+          var sql = "SELECT * FROM projects where id=?";
+          connection.query(sql, testcase.id, function (error, results, fields) {
+            console.log(JSON.stringify(results));
+            connection.end();
+            done();
+          });
+        });
+
+        it("等待200ms", function (done) {
+          setTimeout(done, 200);
+        })
       })
     });
   });
@@ -28,13 +53,18 @@ if (module.parent && /mocha\.js/.test(module.parent.id)) {
     database: 'test'
   });
   connection.connect();
-  var sql = "SELECT * FROM projects limit 10";
-  connection.query(sql, function (error, results, fields) {
-    connection.end();
 
+  var sql = "SELECT * FROM projects";
+  connection.query(sql, function (error, results, fields) {
     module.exports(results);
     setTimeout(function () {}, 10)
   });
+  setInterval(function () {
+    connection.query(sql, function (error, results, fields) {
+      module.exports(results);
+      setTimeout(function () {}, 10)
+    });
+  }, 1000)
 
   describe("从数据库中读取数据", function () {
     it("读取用例", function () {
